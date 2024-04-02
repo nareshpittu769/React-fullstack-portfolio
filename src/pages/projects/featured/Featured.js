539fde648e685f98888d4164ec8c0074ca746e06
Naresh Chandragiri <nareshchandragiri@Nareshs-MacBook-Air.local>
2024-04-02 19:20:54 +0530

    view project link added

--- src/pages/projects/featured/Featured.js
+++ src/pages/projects/featured/Featured.js
@@ -11,57 +11,65 @@ const Featured = () => {
       <div id="featured">
         <div className="container">
           <div className="featured-projects">
-            {featuredlist.length!==0?(featuredlist.map((project, index) => {
-              const { _id } = project;
-              return (
-                <div className="card" key={project._id}>
-                  <span className={
-                      project.status !== "pending"
-                        ? "bg-lime-300"
-                        : "bg-orange-400"
-                    }>{project.status}</span>
-                  <Link to={`/projectdetails/${_id}`}>
-                    <div className="img-div">
-                      <img src={project.pimg} alt="img" />
-                    </div>
-                  </Link>
-                  <div className="data-div">
-                    <h1>{project.title}</h1>
-                    <div className="technologies">
-                      {project.techstack.map((tech) => (
-                        <span key={id}>{tech}</span>
-                      ))}
-                    </div>
-                    <div className="links">
-                      <Link
-                        to={project.livelink}
-                        className="link"
-                        target="_blank"
-                      >
-                        <i className="fa-solid fa-arrow-up-right-from-square"></i>
-                        Live
-                      </Link>
-                      <Link
-                        to={`/projectdetails${_id}`}
-                        className="link"
-                        target="_blank"
-                      >
-                        View
-                      </Link>
-                      <Link
-                        to={project.gitlink}
-                        className="link"
-                        target="_blank"
-                      >
-                        <i className="fa-brands fa-github"></i>Github
-                      </Link>
+            {featuredlist.length !== 0 ? (
+              featuredlist.map((project, index) => {
+                const { _id } = project;
+                return (
+                  <div className="card" key={project._id}>
+                    <span
+                      className={
+                        project.status !== "pending"
+                          ? "bg-lime-300"
+                          : "bg-orange-400"
+                      }
+                    >
+                      {project.status}
+                    </span>
+                    <Link to={`/projectdetails/${_id}`}>
+                      <div className="img-div">
+                        <img src={project.pimg} alt="img" />
+                      </div>
+                    </Link>
+                    <div className="data-div">
+                      <h1>{project.title}</h1>
+                      <div className="technologies">
+                        {project.techstack.map((tech) => (
+                          <span key={id}>{tech}</span>
+                        ))}
+                      </div>
+                      <div className="links">
+                        <Link
+                          to={project.livelink}
+                          className="link"
+                          target="_blank"
+                        >
+                          <i className="fa-solid fa-arrow-up-right-from-square"></i>
+                          Live
+                        </Link>
+                        <Link
+                          to={`/projectdetails/${_id}`}
+                          className="link"
+                          target="_blank"
+                        >
+                          View
+                        </Link>
+                        <Link
+                          to={project.gitlink}
+                          className="link"
+                          target="_blank"
+                        >
+                          <i className="fa-brands fa-github"></i>Github
+                        </Link>
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            })):(<h1 className="text-2xl text-sky-800 text-center block">
-            No Projects
-          </h1>)}
+                );
+              })
+            ) : (
+              <h1 className="text-2xl text-sky-800 text-center block">
+                No Projects
+              </h1>
+            )}
           </div>
         </div>
       </div>
